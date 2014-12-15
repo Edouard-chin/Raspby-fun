3418f8965acdb22644382ed7b22d9d1b994f4be4
Edouard CHIN <echin@u-pro.fr>
2014-12-16 00:35:58 +0100

    Quick and dirty :D

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $client = new \Github\Client();
 $client->authenticate($accessToken, null, Github\Client::AUTH_HTTP_TOKEN);
 $openedPullRequests = $client->api('pull_request')->all('upro', 'digitalevent');
 $pullRequestsDetails = array();
+$infos = array();
 $ttsLang = array(
 	'en',
 	'ja',
@@ -21,11 +22,15 @@ foreach ($pullRequestsDetails as $value) {
 		'user' => $value['user']['login'],
 		'mergeable' => $value['mergeable'],
 		'commits' => $value['commits'],
+		'updatedAt' => strtotime($value['updated_at']),
 	);
 }
-
+if (empty($infos)) {
+	return ;
+} elseif (time() - $infos[0]['updatedAt'] > 90) {
+	return;
+}
 $mergeable = ($infos[0]['mergeable']) ? 'mergeable': 'non mergeable';
-$text = urlencode($infos[0]['user'] . ' a ouvert une poule request ' . $mergeable . ' contenant ' . $infos[0]['commits'] . ' commits');
+$text = urlencode($infos[0]['user'] . ' a ouvert une poule request ' . $mergeable . ' sur digital event, contenant ' . $infos[0]['commits'] . ' commits');
 
 $output = shell_exec("./speech.sh {$text} {$ttsLang[array_rand($ttsLang)]}");
-var_dump($output);exit();
