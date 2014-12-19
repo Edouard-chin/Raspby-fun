5c80e6312fff750f28736e4f5e0b84643b9a138f
Edouard CHIN <echin@u-pro.fr>
2014-12-19 16:31:46 +0100

    WIP

--- index.php
+++ index.php
@@ -10,28 +10,31 @@ $openedPullRequests = $client->api('pull_request')->all('upro', 'digitalevent');
 $pullRequestsDetails = array();
 $infos = array();
 $ttsLang = array(
-	'en',
-	'ja',
-	'de',
-	'tr',
+    'en',
+    'ja',
+    'de',
+    'tr',
 );
 foreach ($openedPullRequests as $value) {
-	$pullRequestsDetails[] = $client->api('pull_request')->show('upro', 'digitalevent', $value['number']);
+    $pullRequestsDetails[] = $client->api('pull_request')->show('upro', 'digitalevent', $value['number']);
 }
 foreach ($pullRequestsDetails as $value) {
-	$infos[] = array(
-		'user' => $value['user']['login'],
-		'mergeable' => $value['mergeable'],
-		'commits' => $value['commits'],
-		'updatedAt' => strtotime($value['updated_at']),
-	);
+    $infos[] = array(
+        'user' => $value['user']['login'],
+        'mergeable' => $value['mergeable'],
+        'commits' => $value['commits'],
+        'updatedAt' => strtotime($value['updated_at']),
+    );
 }
+
+var_dump($infos[0]['updatedAt']);exit();
 if (empty($infos)) {
-	return ;
+    return ;
 } elseif (time() - $infos[0]['updatedAt'] > 90) {
-	return;
+    // return;
 }
 $mergeable = ($infos[0]['mergeable']) ? 'mergeable': 'non mergeable';
 $text = urlencode($infos[0]['user'] . ' a ouvert une poule request ' . $mergeable . ' sur digital event, contenant ' . $infos[0]['commits'] . ' commits');
 
-$output = shell_exec("./speech.sh {$text} {$ttsLang[array_rand($ttsLang)]}");
+putenv("SHELL=xterm");
+$output = shell_exec("/home/echin/exos/Raspby-fun/speech.sh {$text} {$ttsLang[array_rand($ttsLang)]}");
--- speech.sh
+++ speech.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=$2&q=Alexander+you+have+one+niou+poule+requeste"; }
+index=$(pacmd list-sink-inputs | grep index | awk '/index/{print $NF}')
+pacmd set-sink-input-volume $index 0x1000
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols -volume 100 "http://translate.google.com/translate_tts?tl=$2&q=Alexander+you+have+one+niou+poule+requeste"; }
 say $*
 sleep 2
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=fr&q=$1"; }
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols -volume 100 "http://translate.google.com/translate_tts?tl=fr&q=$1"; }
 say $*
+pacmd set-sink-input-volume $index 0x10000
