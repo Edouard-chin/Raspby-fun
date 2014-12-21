0a4f952d3b2f114513ab4bdf72075b6eb2e7f963
Edouard CHIN <echin@u-pro.fr>
2014-12-21 17:04:29 +0100

    Added a jingle before the annoucement.

--- speech.sh
+++ speech.sh
@@ -4,6 +4,7 @@ if ! [ -z "$index" ]; then
 	#pacmd set-sink-input-volume $index 0x1000
 	sleep 10
 fi
+aplay jingle.wav
 pico2wave --lang $2 -w play.wav "$1" && aplay play.wav
 # if ! [ -z "$index" ]; then
 # 	pacmd set-sink-input-volume $index 0x10000
