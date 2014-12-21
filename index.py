4afc742756b05dc6947ca585229fcacf8d925a5b
Edouard CHIN <echin@u-pro.fr>
2014-12-21 16:30:28 +0100

    Created random sentences. Now forking the speech process, in order to not be timed out when command is launching from a web cron task.

--- index.py
+++ index.py
@@ -5,13 +5,16 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-from flask import Flask
+from flask import Flask, abort
 from flask import request
 from subprocess import call
 import json
+import random
+import os
 
 app = Flask(__name__)
 users = {'Edouard-chin': 'édouard', 'rlustin': 'Rafhael', 'aldeck': 'Alexandre', 'cthoret': 'Clément', 'alexandra-ekra': 'Alexandra', 'jvasseur': 'Jérome', 'FlorianLepot': 'Florian'}
+
 
 @app.route("/payload/pullrequest", methods=['POST'])
 def pullrequest():
@@ -21,18 +24,18 @@ def pullrequest():
     isMergeable = {True: 'can', False: "can not", None: 'I am not sure if it can'}
     pullrequest = data['pull_request']
     if state == 'opened' or state == 'reopened':
-        speech = 'New pull request on: ' + pullrequest['base']['repo']['name'] + '. ' + users[pullrequest['user']['login']] + ' want to merge his branch: ' \
+        speech = 'New pull request on: ' + pullrequest['base']['repo']['name'] + '. ' + users[pullrequest['user']['login']] + ' wants to merge his branch: ' \
         + pullrequest['head']['ref'] + ', in the ' + pullrequest['base']['ref'] + ' branch, containing ' + str(pullrequest['commits']) + ' commit. This pull request ' \
         + isMergeable[pullrequest['mergeable']] + ' be automatically merged.'
-        call(["./speech.sh", speech])
+        forkMe(speech, 'en-US')
 
     elif state == 'closed':
         speech = 'The pull request of: ' + users[pullrequest['user']['login']] + ' is now closed and was ' + wasMerged[pullrequest['merged']]
-        call(["./speech.sh", speech])
+        forkMe(speech, 'en-US')
 
     elif state == 'assigned':
         speech = users[pullrequest['assignee']['login']] + ', you have been assigned to check the pull request of: ' + users[pullrequest['user']['login']]
-        call(["./speech.sh", speech])
+        forkMe(speech, 'en-US')
 
     return "All good!"
 
@@ -44,6 +47,46 @@ def pull_review():
     speech = users[pullrequest['user']['login']] + ', ' + users[comment['user']['login']] + ' commented on your pull request. Go check it.'
     call(["./speech.sh", speech])
     return 'All good!'
+
+@app.route("/cron/good-morning", methods=['GET'])
+def cron_goodMorning():
+    if not check_ip():
+        abort(401)
+    return forkMe('Bonjour tout le monde. Il est 9 heures 45, passez une bonne journée.', 'fr-FR')
+
+@app.route("/cron/time-to-eat", methods=['GET'])
+def cron_timeToEat():
+    store = ['Sumita', 'Luigi', 'Picard', 'Kebab', 'Subway', 'Repas a L\'anjou', 'Repas au café L\'express']
+    if not check_ip():
+        abort(401)
+    return forkMe("Il va bientot être l'heure de manger. Je vous propose aujourd'hui d'aller se faire un: " + random.choice(store), 'fr-FR')
+
+
+@app.route("/cron/random", methods=['GET'])
+def cron_random():
+    if not check_ip():
+        abort(401)
+    json_data=open('randomSentences.json')
+    data = json.load(json_data)
+    speech = random.choice(data['sentences'])
+    json_data.close()
+    return forkMe(speech, 'fr-FR')
+
+def speak(sentence, lang):
+    call(["./speech.sh", sentence, lang])
+    os._exit(0)
+
+def forkMe(sentence, lang):
+    newpid = os.fork()
+    if newpid == 0:
+         speak(sentence, lang)
+    else:
+        return "All good marmoud!"
+
+def check_ip():
+    allowedIps = ['142.4.201.54', '198.27.81.134', '127.0.0.1', '89.156.7.131']
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    return ip in allowedIps
 
 if __name__ == "__main__":
     app.run()
--- speech.sh
+++ speech.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 index=$(pacmd list-sink-inputs | grep index | awk '/index/{print $NF}')
 if ! [ -z "$index" ]; then
-	pacmd set-sink-input-volume $index 0x1000
+	#pacmd set-sink-input-volume $index 0x1000
+	sleep 10
 fi
-pico2wave -w play.wav "$*" && aplay play.wav
-echo 'good'
-if ! [ -z "$index" ]; then
-	pacmd set-sink-input-volume $index 0x10000
-fi
+pico2wave --lang $2 -w play.wav "$1" && aplay play.wav
+# if ! [ -z "$index" ]; then
+# 	pacmd set-sink-input-volume $index 0x10000
+# fi
