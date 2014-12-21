22e828371657ffa24625224ef53fbba8ff6d4370
Edouard CHIN <echin@u-pro.fr>
2014-12-21 02:49:45 +0100

    Now using github webhooks. Recreated the whole code.

---
+++ index.py
@@ -0,0 +1,48 @@
+# coding=UTF-8
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+from flask import Flask
+from flask import request
+from subprocess import call
+import json
+
+app = Flask(__name__)
+users = {'Edouard-chin': 'édouard', 'rlustin': 'Rafhael', 'aldeck': 'Alexandre', 'cthoret': 'Clément', 'alexandra-ekra': 'Alexandra', 'jvasseur': 'Jérome', 'FlorianLepot': 'Florian'}
+
+@app.route("/payload/pullrequest", methods=['POST'])
+def pullrequest():
+    data = json.loads(request.data)
+    state = data['action']
+    wasMerged = {True: 'merged', False: 'not merged'}
+    isMergeable = {True: 'can', False: "can not", None: 'I am not sure if it can'}
+    pullrequest = data['pull_request']
+    if state == 'opened' or state == 'reopened':
+        speech = 'New pull request on: ' + pullrequest['base']['repo']['name'] + '. ' + users[pullrequest['user']['login']] + ' want to merge his branch: ' \
+        + pullrequest['head']['ref'] + ', in the ' + pullrequest['base']['ref'] + ' branch, containing ' + str(pullrequest['commits']) + ' commit. This pull request ' \
+        + isMergeable[pullrequest['mergeable']] + ' be automatically merged.'
+        call(["./speech.sh", speech])
+
+    elif state == 'closed':
+        speech = 'The pull request of: ' + users[pullrequest['user']['login']] + ' is now closed and was ' + wasMerged[pullrequest['merged']]
+        call(["./speech.sh", speech])
+
+    elif state == 'assigned':
+        speech = users[pullrequest['assignee']['login']] + ', you have been assigned to check the pull request of: ' + users[pullrequest['user']['login']]
+        call(["./speech.sh", speech])
+
+    return "All good!"
+
+@app.route("/payload/pullrequest-review", methods=['POST'])
+def pull_review():
+    data = json.loads(request.data)
+    pullrequest = data['pull_request']
+    comment = data['comment']
+    speech = users[pullrequest['user']['login']] + ', ' + users[comment['user']['login']] + ' commented on your pull request. Go check it.'
+    call(["./speech.sh", speech])
+    print speech
+    return 'All good!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- speech.sh
+++ speech.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 index=$(pacmd list-sink-inputs | grep index | awk '/index/{print $NF}')
-pacmd set-sink-input-volume $index 0x1000
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols -volume 100 "http://translate.google.com/translate_tts?tl=$2&q=Alexander+you+have+one+niou+poule+requeste"; }
-say $*
-sleep 2
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols -volume 100 "http://translate.google.com/translate_tts?tl=fr&q=$1"; }
-say $*
-pacmd set-sink-input-volume $index 0x10000
+if ! [ -z "$index" ]; then
+	pacmd set-sink-input-volume $index 0x1000
+fi
+pico2wave -w lookdave.wav "$*" && aplay lookdave.wav
+echo 'good'
+if ! [ -z "$index" ]; then
+	pacmd set-sink-input-volume $index 0x10000
+fi
