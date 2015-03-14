440352de429b0d51a05790e988c41c2c2ddbae54
Edouard CHIN <echin@u-pro.fr>
2015-03-14 01:56:40 +0100

    The condition was incorrect

--- volume.sh
+++ volume.sh
@@ -5,9 +5,10 @@ say() {
 }
 
 sinkIndexes=($(pacmd list-sink-inputs | grep index | awk '/index/{print $NF}'))
-if [ ! -z "{$sinkIndexes[0]}" ]; then
+if [ ! ${#sinkIndexes[@]} -eq 0 ]; then
 	say "$2"
 fi
+exit
 for i in "${sinkIndexes[@]}"; do
     pactl set-sink-input-volume -- $i $1
 done
