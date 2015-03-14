56737eedf27a5fe03bd11a86d080733169d1c930
Edouard CHIN <echin@u-pro.fr>
2015-03-14 11:34:03 +0100

    Shorter delay between song annoucment and song playing

--- pickasong.sh
+++ pickasong.sh
@@ -10,17 +10,17 @@
 # 	done
 # }
 
-pico2wave --lang "fr-FR" -w play.wav "La chanson espagnole du jour" && aplay play.wav
+mpc -q clear
+mpc -q findadd "any" "spanish"
+mpc -q shuffle
 declare -a args
 count=0
 for arg in "Mais laisser moi dabord baisser cette musique nulle"; do
   args[count]=$(printf '%q' "$arg")
   count=$((count+1))
 done
+pico2wave --lang "fr-FR" -w play.wav "La chanson espagnole du jour" && aplay play.wav
 ssh dudek@192.168.0.11 'bash -s' < volume.sh -- "-50%" "${args[@]}"
-mpc -q clear
-mpc -q findadd "any" "spanish"
-mpc -q shuffle
 mpc -q play 10
 mpc -q crop
 sleep 10
